c96f7cf2cd18a282725b0843879c9166798100a8
KevsonDoc <kevsonfilipesantos@gmail.com>
2020-07-30 22:01:53 -0300

    [add] - 404

--- src/Pages/Err/index.js
+++ src/Pages/Err/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import Menu from '../../components/Menu';
 import Video from '../../assets/Video/video.mp4';
+import './style.css';
 
 function Err() {
   return (
       <>
         <Menu />
-        <video className="bg_video" autoplay loop>
-	        <source src={Video} type="video/mp4" />
-        </video>	
+        <div className="container_404">
+          <h1 className="title_1">Erro 404</h1>
+          <h1 className="title_2">Eu conseguir encontrar o que você procura</h1>
+          <video autoPlay className="bg_video" loop>
+	          <source src={Video} type="video/mp4" />
+          </video>
+        </div>
       </>
   );
 }
